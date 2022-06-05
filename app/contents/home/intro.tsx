2778d06e566a9037b6406db27fa49337414611f4
M Haidar Hanif <git@mhaidarhanif.com>
2022-06-06 02:57:15 +0700

    feat(style): home intro center

--- app/contents/home/intro.tsx
+++ app/contents/home/intro.tsx
@@ -1,14 +1,14 @@
 export const HomeIntro = () => {
   return (
     <header>
-      <h1 className="flex flex-wrap">
+      <h1 className="flex flex-wrap justify-center text-center">
         <span>
           <span className="text-primary-500">No Code</span>
           <span> Backend API,</span>
         </span>
         <span>Fast and Easy!</span>
       </h1>
-      <p>
+      <p className="text-center">
         Kontenbase allows you to easily create backend API, auth, and storage in
         less than one minute without coding.
       </p>
