aa8d2c8ef70fecead942aa6f0cf13b00737ffb78
M Haidar Hanif <git@mhaidarhanif.com>
2022-07-02 13:12:43 +0700

    feat: optimize home intro font size

--- app/contents/home/intro.tsx
+++ app/contents/home/intro.tsx
@@ -3,18 +3,15 @@ import { ButtonGroupCTA } from "~/contents";
 export const HomeIntro = () => {
   return (
     <section className="stack-v layout-padding items-center gap-10">
-      <div className="layout-content small md:medium">
-        <h1 className="h0 special">
-          <span>
-            <span className="text-primary-500">No Code</span>
-            <span> Backend API,</span>
-          </span>
-          <span>Fast and Easy!</span>
+      <div className="layout-content small md:medium flex justify-center">
+        <h1 className="h0 special max-w-[415px] sm:max-w-[680px]">
+          <span className="text-primary-500">No Code </span>
+          Backend API, Fast and Easy!
         </h1>
       </div>
 
       <div className="layout-content small">
-        <p className="text-center text-xl">
+        <p className="text-center text-lg sm:text-xl">
           Kontenbase allows you to easily create backend API, auth, and storage
           in less than one minute without coding.
         </p>
