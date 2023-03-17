ac0d6f279edc32c4ff435c1e72b1e504eb853fc1
Muhamad Chairul Aziz <m.chairul669@gmail.com>
2023-03-18 00:30:57 +0700

    fix: update feature preview
    - carousel in hero
    - video preview for highlighted features

---
+++ app/data/intro-carousel.ts
@@ -0,0 +1,17 @@
+export const carouselList = [
+  {
+    id: 0,
+    image: "/assets/images/intro/data.png",
+    label: "Data",
+  },
+  {
+    id: 1,
+    image: "/assets/images/intro/app-builder.png",
+    label: "App Builder",
+  },
+  {
+    id: 2,
+    image: "/assets/images/intro/automation.png",
+    label: "Automation",
+  },
+];
