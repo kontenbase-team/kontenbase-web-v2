d816423d0f13b1e3a381b92de0989fba30b84e23
Muhamad Chairul Aziz <m.chairul669@gmail.com>
2023-03-21 16:32:06 +0700

    fix: video preview auto play & loop

--- app/components/video.tsx
+++ app/components/video.tsx
@@ -1,3 +1,11 @@
+// eslint-disable-next-line import/order
+import { useCallback } from "react";
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import YouTube from "react-youtube";
+
+import type { YouTubeEvent } from "react-youtube";
+
 interface VideoYouTubeProps {
   title: string;
   embedId: string;
@@ -11,17 +19,46 @@ export const VideoYouTube = ({
   autoPlay,
   loop,
 }: VideoYouTubeProps) => {
+  const opts = {
+    width: "100%",
+    height: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: Number(autoPlay),
+      mute: Number(autoPlay),
+      loop: Number(loop),
+    },
+  };
+
+  const handleAutoPlay = useCallback(
+    ({ target }: YouTubeEvent<any>) => {
+      if (autoPlay) {
+        target.playVideo();
+      }
+    },
+    [autoPlay],
+  );
+
+  const handleLoop = useCallback(
+    ({ data, target }: YouTubeEvent<number>) => {
+      if (loop) {
+        if (data === YouTube.PlayerState.ENDED) {
+          target.playVideo();
+        }
+      }
+    },
+    [loop],
+  );
+
   return (
-    <iframe
-      className="border-panel aspect-video w-full rounded-lg shadow-lg"
+    <YouTube
+      id={embedId}
+      videoId={embedId}
+      className="card-shadow aspect-video w-full overflow-hidden rounded-lg p-0 shadow-lg"
       title={title}
-      src={`https://www.youtube.com/embed/${embedId}?playlist=${embedId}&controls=1&modestbranding=1&rel=0&disablekb=1${
-        autoPlay ? "&autoplay=1&mute=1" : ""
-      }${loop ? "&loop=1" : ""}`}
-      frameBorder="0"
-      allowFullScreen
-      style={{ border: "none" }}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+      opts={opts}
+      onReady={handleAutoPlay}
+      onStateChange={handleLoop}
     />
   );
 };
